d471a15aae70fe787b03cde68d515ffbe3af0508
applebear <528046@qq.com>
2018-03-04 17:25:50 +0800

    note: dev-3-4-1

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 const Koa = require('koa')
 const app = new Koa()
+const views = require('koa-views')
+const static = require('koa-static')
+const path = require('path')
+const queryString = require('querystring')
+
+app.use(static(path.resolve(__dirname, './assets')))
+
+app.use(views(path.resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-  ctx.body = 'hi lxh!!'
+  if(ctx.path == '/favicon.ico') return
+  console.log(queryString.parse(ctx.search.substr(1)))
+  await ctx.render('index', {
+    title: '苹果熊',
+    name: '吖吖!!'
+  })
 })
 
 app.listen(1234)
